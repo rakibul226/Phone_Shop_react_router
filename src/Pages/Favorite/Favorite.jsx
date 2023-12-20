4924512cd433a5c83287d7be614d447d0c9822ee
“rakibul8226” <rakibul8226@gmail.com>
2023-12-21 04:12:50 +0600

    get item from local storage

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
+import FavoriteCard from "./FavoriteCard";
 
 
 const Favorite = () => {
+    const [favorites,setFavorite] = useState([]);
+    const [noDataFound,setNoDataFound] = useState(false);
+
+    useEffect( ()=>{
+
+        const favoriteItems = JSON.parse(localStorage.getItem('favorites'))
+        
+        if(favoriteItems){
+            setFavorite(favoriteItems);
+        }else{
+            setNoDataFound("No Data Found");
+        }
+    },[])
+
+
     return (
         <div>
-            Favorite page
+          {noDataFound ? (
+            <p>{noDataFound}</p>
+          ) : (
+            <div>
+              <h2 className="text-2xl text-center">All category phones</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {favorites.map((phone) => (
+                  <FavoriteCard phone={phone} />
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-    );
+      );
+      
 };
 
 export default Favorite;
---
+++ src/Pages/Favorite/FavoriteCard.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+const FavoriteCard = ({phone}) => {
+    const {id,image,phone_name,brand_name,price} = phone || {}
+    return (
+        <div className="relative flex flex-col text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl w-96 mb-2">
+  <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
+    <img
+      src={image}
+      alt="card-image" className="object-cover w-full h-full" />
+  </div>
+  <div className="p-6">
+    <div className="flex items-center justify-between ">
+      <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+        {phone_name}
+      </p>
+      <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+      ${price}
+      </p>
+    </div>
+    <div className="mb-2 font-bold">
+        {brand_name}
+    </div>
+    <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
+      With plenty of talk and listen time, voice-activated Siri access, and an
+      available wireless charging case.
+    </p>
+  </div>
+  <div className="p-6 pt-0">
+
+   <Link>
+   <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 focus:opacity-[0.85] active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100 bg-gray-300"
+      type="button">
+      Add to cart
+    </button>
+   </Link>
+
+  </div>
+</div>
+    );
+};
+
+export default FavoriteCard;

4c28aa7e2ba2cab561845156ca04a0cdc20d8665
“rakibul8226” <rakibul8226@gmail.com>
2023-12-25 03:15:53 +0600

    reove from local storage

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import FavoriteCard from "./FavoriteCard";
 
 
+
+
+
 const Favorite = () => {
     const [favorites,setFavorite] = useState([]);
     const [noDataFound,setNoDataFound] = useState(false);
 
     useEffect( ()=>{
-
         const favoriteItems = JSON.parse(localStorage.getItem('favorites'))
         
         if(favoriteItems){
@@ -17,6 +19,11 @@ const Favorite = () => {
         }
     },[])
 
+    const handleRemove = ()=>{
+      localStorage.clear();
+      setFavorite([]);
+      setNoDataFound("No data found")
+    }
 
     return (
         <div>
@@ -24,17 +31,16 @@ const Favorite = () => {
             <p>{noDataFound}</p>
           ) : (
             <div>
-              <h2 className="text-2xl text-center">All category phones</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {favorites.length > 0 && <button onClick={handleRemove} className="px-5 py-2 bg-red-400 block mx-auto my-2 rounded-sm text-white">Delete All Item</button>}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
                 {favorites.map((phone) => (
                   <FavoriteCard phone={phone} />
                 ))}
               </div>
             </div>
           )}
-        </div>
-      );
-      
+        </div> 
+    );
 };
 
 export default Favorite;

b4c336575ea9c47344cb7634c5168cfb19b2e199
“rakibul8226” <rakibul8226@gmail.com>
2023-12-25 03:46:25 +0600

    Add See More/less button

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -8,6 +8,7 @@ import FavoriteCard from "./FavoriteCard";
 const Favorite = () => {
     const [favorites,setFavorite] = useState([]);
     const [noDataFound,setNoDataFound] = useState(false);
+    const [isShow,setIsShow] = useState(false);
 
     useEffect( ()=>{
         const favoriteItems = JSON.parse(localStorage.getItem('favorites'))
@@ -33,10 +34,17 @@ const Favorite = () => {
             <div>
               {favorites.length > 0 && <button onClick={handleRemove} className="px-5 py-2 bg-red-400 block mx-auto my-2 rounded-sm text-white">Delete All Item</button>}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-                {favorites.map((phone) => (
-                  <FavoriteCard phone={phone} />
-                ))}
+                {
+                  isShow ? favorites.map((phone) => (
+                    <FavoriteCard phone={phone} key ={phone.id} />
+                  ))
+                  :
+                  favorites.slice(0,2).map((phone) => (
+                    <FavoriteCard phone={phone} key ={phone.id}/>
+                  ))
+                }
               </div>
+              <button onClick={()=>setIsShow(!isShow)} className="px-5 py-2 bg-red-400 block mx-auto my-2 rounded-sm text-white">{isShow ? "See less" : "See more"}</button>
             </div>
           )}
         </div> 
